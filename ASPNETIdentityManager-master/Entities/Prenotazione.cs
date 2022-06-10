231335df6451a8699a3bb53651dd9d8335531c1a
tdpc13 <106995904+tdpc13@users.noreply.github.com>
2022-06-10 21:16:02 +0200

    Add files via upload

---
+++ ASPNETIdentityManager-master/Entities/Prenotazione.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ASPNETIdentityManager.Entities
+{
+    public class Prenotazione
+    {
+        public int Id { get; set; }
+        public Guid IdUser { get; set; }
+        public string Dal { get; set; }
+        public string Al { get; set; }
+
+        public int Persone { get; set; }
+
+        public string Pacchetto { get; set; }
+    }
+}
